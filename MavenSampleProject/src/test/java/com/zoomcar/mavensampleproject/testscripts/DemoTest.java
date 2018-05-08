2704e4089148b970937a3ec0eef9eccfec68020e
tyss <tyss@198.105.254.11>
2018-05-08 12:15:17 +0530

    First Commit

---
+++ MavenSampleProject/src/test/java/com/zoomcar/mavensampleproject/testscripts/DemoTest.java
@@ -0,0 +1,11 @@
+package com.zoomcar.mavensampleproject.testscripts;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void create()
+	{
+		System.out.println("Running create(). . .");
+	}
+}
